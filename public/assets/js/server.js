74e53559be033ee9bfe8c05dea73dcb77d17571d
Jacob <Jakelausier7878@gmail.com>
2023-07-28 11:37:25 -0400

    added route to maine html, and route to read existing notes

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -3,3 +3,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+// Route to serve the main HTML page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+  
+  // Route to read existing notes from the JSON file
+  app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to read notes' });
+      }
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
