5e1ddddc706be64351d3a4ca4bda7a7aa399dcda
Jacob <Jakelausier7878@gmail.com>
2023-07-28 13:44:17 -0400

    added the route for note creation and note deletion. moving to testing server

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,21 +1,80 @@
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Route to serve the main HTML page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Route to read existing notes from the JSON file
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
   });
-  
-  // Route to read existing notes from the JSON file
-  app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db', 'db.json'), 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Failed to read notes' });
+});
+// Route to handle new note creation
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  if (!title || !text) {
+    return res.status(400).json({ error: "Title and text are required" });
+  }
+
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = { title, text, id: notes.length + 1 };
+    notes.push(newNote);
+
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Failed to save note" });
+        }
+        res.json(newNote);
       }
-      const notes = JSON.parse(data);
-      res.json(notes);
-    });
+    );
   });
+});
+
+// Route to handle note deletion
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = parseInt(req.params.id);
+
+  fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Failed to read notes" });
+    }
+
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== noteId);
+
+    fs.writeFile(
+      path.join(__dirname, "db", "db.json"),
+      JSON.stringify(notes),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Failed to delete note" });
+        }
+        res.json({ message: "Note deleted successfully" });
+      }
+    );
+  });
+});
