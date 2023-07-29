343dac665f62a0adc66f066ee8a87ccd6cba7fe1
Jacob <jakelausier7878@gmail.com>
2023-07-28 20:38:23 -0400

    added required files and got index html to work, error loading notes html

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
-const PORT = process.env.PORT || 3001;
+const PORT =  3001
 // Route to serve the main HTML page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -12,6 +16,8 @@ app.get("/", (req, res) => {
 
 // Route to read existing notes from the JSON file
 app.get("/api/notes", (req, res) => {
+  
+
   fs.readFile(path.join(__dirname, "db", "db.json"), "utf8", (err, data) => {
     if (err) {
       console.error(err);
